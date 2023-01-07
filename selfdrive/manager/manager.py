0e04176e3af06a516ab56ed487d922afddfb9f7b
Jason Wen <haibin.wen3@gmail.com>
2023-01-06 22:33:03 -0500

    expose DisableRadar

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -40,6 +40,7 @@ def manager_init() -> None:
     ("CompletedTrainingVersion", "0"),
     ("HasAcceptedTerms", "0"),
     ("OpenpilotEnabledToggle", "1"),
+    ("DisableRadar_Allow", "1"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
